5e9ccce9d9097914b0571910b49a82b4b7426ecf
Matt David <mcdav3636@gmail.com>
2017-10-07 11:22:28 -0600

    added and edited content

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
 <!DOCTYPE html>
 <html>
 	<head>
+<meta charset="utf-8">
 		<title>MacDrones Home Page</title>
-		<h1>MacDrones</h1>
 	</head>
 	<body>
+		<h1>MacDrones</h1>
 		<h2>The Best View Yet</h2>
 		<p>MacDrones is your eyes in the sky capturing the views that used to be just out of reach.
-		Think of all you have been missing!</p>
+			Not only can you see what you have never seen before but MacDrones biggest benefit is <strong>Saving Money</strong>
+			through time efficiency, safety, quality controle, and accuracy.
+			Think of all you have been missing!</p>
+		<div>
+			<h2>The Possabilities are Endless</h2>
+			<p>We</p>
 		<ul>
 			<li>Beautiful landscapes</li>
 			<li>Full Realestate Video</li>
 			<li>action video</li>
 		</ul>
-
+		</div>
 	</body>
 </html>
