e4c164fd8e0e678ff561f18ce45945b1b6d198e7
Matt David <mcdav3636@gmail.com>
2017-10-07 08:59:08 -0600

    Started the page. Created simple mark ups

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>MacDrones Home Page</title>
+		<h1>MacDrones</h1>
+	</head>
+	<body>
+		<h2>The Best View Yet</h2>
+		<p>MacDrones is your eyes in the sky capturing the views that used to be just out of reach.
+		Think of all you have been missing!</p>
+		<ul>
+			<li>Beautiful landscapes</li>
+			<li>Full Realestate Video</li>
+			<li>action video</li>
+		</ul>
+
+	</body>
+</html>
